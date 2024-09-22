483f902e6fbd2f5a57782ce252192cbaadcfb055
Marco <Marco@TOKYO>
2024-09-22 23:47:42 +0200

    Adjusted default Config values

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -5,13 +7,17 @@ class Config(object):
     API_TITLE = 'user-service'
     OPENAPI_VERSION = '3.0.2'
     OPENAPI_URL_PREFIX = '/'
+    URL_PREFIX = '/users/'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
     CORS_ORIGINS = ["*"]
     JWT_SECRET_KEY = "super-secret"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1024)
 class ProductionConfig(Config):
     DATABASE_URI = 'mysql://user@127.0.0.1/users'
 class DevelopmentConfig(Config):
     DEBUG = True
 class TestConfig(Config):
     TESTING = True
+    DEBUG = True
+    PROPAGATE_EXCEPTIONS = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
