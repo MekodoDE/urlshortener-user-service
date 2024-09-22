cb5debf5fbf3349c92dbfca35689a10ddd381d49
Marco <Marco@TOKYO>
2024-09-22 19:20:45 +0200

    Override config via env vars

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ def create_app():
     # Load configuration from environment variable or use default
     CONFIG_TYPE = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
     app.config.from_object(CONFIG_TYPE)
+    app.config.from_mapping(os.environ)
 
     # Enable Cross-Origin Resource Sharing (CORS) with origins specified in configuration
     CORS(app, origins=app.config['CORS_ORIGINS'])
