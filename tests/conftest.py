48cd794689da7742332a09097ecb800c6122c2d7
Marco <Marco@TOKYO>
2024-10-06 22:05:23 +0200

    Add role test with admin access

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 import os
+from flask_jwt_extended import create_access_token
 from app import create_app
+from app.models.user import UserModel
+from app.views.utils import hash_password
 from app.extensions.database import db
 
 db_path = "./instance/test.db"
@@ -10,26 +13,42 @@ class UserData:
     username = "testuser"
     password = "testpassword"
     email = "testuser@example.com"
+    role = "member"
     access_token = None
 
+class AdminUserData:
+    id = "18159319-08e4-478b-9ff6-eecf4bb44ed8"
+    username = "testadmin"
+    password = "testpassword"
+    email = "testadmin@example.com"
+    role = "admin"
+    access_token = None
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def app():
     _app = create_app()
 
     with _app.app_context():
+        user = UserModel(id=AdminUserData.id, username=AdminUserData.username, email=AdminUserData.email, password=hash_password(AdminUserData.password), role=AdminUserData.role)
+        db.session.add(user)
         yield _app
         db.session.remove()
         db.engine.dispose()
         if os.path.exists(db_path):
             os.remove(db_path)
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def client(app):
     return app.test_client()
 
-@pytest.fixture(scope='session')
-def headers(client):
+@pytest.fixture(scope="session")
+def headers():
     return {
         'Authorization': f'Bearer {UserData.access_token}'
+    }
+
+@pytest.fixture(scope="session")
+def admin_headers():
+    return {
+        "Authorization": f"Bearer {AdminUserData.access_token}"
     }
--- tests/test_http_response.py
+++ tests/test_http_response.py
@@ -1,5 +1,5 @@
 import pytest
-from .conftest import UserData
+from .conftest import UserData, AdminUserData
 
 url_prefix = '/users'
 
@@ -34,7 +34,22 @@ def test_login(client):
     assert 'access_token' in data
     UserData.access_token = data['access_token']  # Save token for authenticated requests
 
-def test_all_user_list(client, headers):
+def test_admin_login(client):
+    """
+    Test admin login.
+    Checks that login returns a JWT access token.
+    """
+    login_data = {
+        'username': AdminUserData.username,
+        'password': AdminUserData.password
+    }
+    response = client.post(url_prefix + '/login', json=login_data)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert 'access_token' in data
+    AdminUserData.access_token = data['access_token']  # Save token for authenticated requests
+
+def test_get_users(client, headers):
     """
     Test access to user list.
     Verifies that access is forbidden without proper credentials.
@@ -42,12 +57,40 @@ def test_all_user_list(client, headers):
     response = client.get(url_prefix + '/', headers=headers)
     assert response.status_code == 403
 
+def test_admin_get_users(client, admin_headers):
+    """
+    Test access to user list.
+    Verifies that access is allowed with admin credentials.
+    """
+    response = client.get(url_prefix + '/', headers=admin_headers)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert len(data) == 2
+
 def test_get_user(client, headers):
     """
     Test retrieving user details.
     Verifies that correct user details are returned.
     """
-    response = client.get(f'/{UserData.id}', headers=headers)
+    response = client.get(url_prefix + f'/{UserData.id}', headers=headers)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['username'] == UserData.username
+
+def test_get_other_user(client, headers):
+    """
+    Test retrieving other users details.
+    Verifies that access is not allowed.
+    """
+    response = client.get(url_prefix + f'/{AdminUserData.id}', headers=headers)
+    assert response.status_code == 403
+
+def test_admin_get_user(client, admin_headers):
+    """
+    Test retrieving user details.
+    Verifies that correct user details are returned.
+    """
+    response = client.get(url_prefix + f'/{UserData.id}', headers=admin_headers)
     assert response.status_code == 200
     data = response.get_json()
     assert data['username'] == UserData.username
@@ -61,6 +104,19 @@ def test_update_user(client, headers):
         'email': 'newemail@example.com'
     }
     response = client.put(url_prefix + f'/{UserData.id}', json=update_data, headers=headers)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['email'] == update_data['email']
+
+def test_admin_update_user(client, admin_headers):
+    """
+    Test updating user information.
+    Verifies that the user's email is updated.
+    """
+    update_data = {
+        'email': 'newemail2@example.com'
+    }
+    response = client.put(url_prefix + f'/{UserData.id}', json=update_data, headers=admin_headers)
     assert response.status_code == 200
     data = response.get_json()
     assert data['email'] == update_data['email']
